0b4f5039f73ab6b45c9bb7c1eb01139eaf81b5ea
psx95 <pranav.ps95@hotmail.com>
2019-04-20 18:00:46 +0530

    Adds BinaryToString - Fraction to Binary

---
+++ Chapter 5 - Bit Manipulation/BinaryToString.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class BinaryToString {
+    public static void main (String Args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eneter the number between 0 and 1");
+        double number = sc.nextDouble();
+        System.out.println(getBinaryRepresentation(number));
+    }
+
+    private static String getBinaryRepresentation (double number) {
+        double temp = number;
+        String ans = ".";
+        int bitCount = 0;
+        while (temp!=0 && bitCount <= 32) {        
+            temp = temp*2;
+            if (temp >= 1) {
+                ans+="1";
+                temp -=  1;            
+            } else {
+                ans+="0";
+            }
+            bitCount++;
+        }        
+        // System.out.println(ans + " \n bitCOunt  "+bitCount);
+        if (bitCount > 32) {
+            return "ERROR";
+        }        
+        return ans;
+    }
+}
